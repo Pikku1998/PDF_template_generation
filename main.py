2103a389bf8908bd5be19be1d6d33cf879a01c68
Prakash R <prakashramanukolanu1998@gmail.com>
2023-01-13 14:57:29 +0530

    Add more pages for each topic

--- main.py
+++ main.py
@@ -11,5 +11,7 @@ for index, row in df.iterrows():
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=row['Topic'], border=0, align="C")
     pdf.line(10, 22, 200, 22)
+    for i in range(row['Pages'] - 1):
+        pdf.add_page()
 
 pdf.output("ProgrammingNotes.pdf")
